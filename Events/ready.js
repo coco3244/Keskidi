edaa67734a046c0bdb8918d847bbfd62464705ad
coco3244 <colin.nemeghaire@hotmail.com>
2024-04-25 17:41:56 +0200

    init

---
+++ Events/ready.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js")
+const loadSlashCommand = require("../Loaders/loadSlashCommands")
+
+module.exports = async(bot) =>{
+
+    await loadSlashCommand(bot)
+    console.log(`${bot.user.tag} online`)
+
+}
--- Keskidi.js
+++ Keskidi.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 require('dotenv').config();
 const intents = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({intents});
+const loadCommands = require("./Loaders/loadCommands")
+const loadEvents = require("./Loaders/loadEvents")
+
+bot.commands = new Discord.Collection()
+bot.color="#ffff"
+bot.connection =[]
 
 bot.login(process.env.TOKEN);
 loadCommands(bot)
+loadEvents(bot)
