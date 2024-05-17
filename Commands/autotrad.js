e7272f59b3d222955a2680fc56638273aca0d1a1
coco3244 <colin.nemeghaire@hotmail.com>
2024-05-17 21:11:38 +0200

    1.1.1

--- Commands/autotrad.js
+++ Commands/autotrad.js
@@ -5,47 +5,56 @@ let Started = false;
 
 module.exports = {
   name: "autotrad",
-  description: "Autotrad du channel",
+  description: "Auto-traduction du canal dans le quel la commande a été exécutée",
   permissions: "Aucune",
   dm: false,
   category: "Action",
 
   run(bot, message) {
-    const listener =  (msg) => {
+    const listener = async (msg) => {
       if (
         msg.author.id != bot.user.id &&
         Channels.includes(msg.channel.id) &&
         msg.content.length <= 2000
       ) {
-        translate(msg.content, { to: "fr" })
+        let Thread
+        await translate(msg.content, { to: "fr" })
           .then(async (res) => {
             if (res.from.language.iso.toLowerCase() == "fr") {
               return;
             } else {
-              const thread = await msg.startThread({
-                name:'Traduction',
+              Thread = await msg.startThread({
+                name: "Traduction",
                 autoArchiveDuration: Discord.ThreadAutoArchiveDuration.OneHour,
-              })
-              thread.send({content: `Réponse : \`\`\`${res.text}\`\`\``})
-              return
+              });
+              await Thread.send({ content: `\`\`\`${res.text}\`\`\``});
+              return;
             }
           })
-          .catch(async (err) => {
-            const thread = await msg.startThread({
-              name:'Traduction (Erreur)',
-              autoArchiveDuration: Discord.ThreadAutoArchiveDuration.OneHour,
-            })
-            thread.send({content: `Réponse : \`\`\`${err}\`\`\``})
-              return
+          .catch( async(err) => {
+            if(Thread==null){
+              Thread = await msg.startThread({
+                name: "Traduction (Erreur)",
+                autoArchiveDuration: Discord.ThreadAutoArchiveDuration.OneHour,
+              });
+            }else{
+              await Thread.setName("Traduction (Erreur)")
+            }
+            if(err.code==50035 && err.rawError.errors.content._errors[0].code == "BASE_TYPE_MAX_LENGTH"){
+              await Thread.send({ content: `\`\`\`Le message fait plus de 2000 caractères et n'a donc pas pu être traduit\nUtilises /tmc et découpe le message en 2 parties (ou plus au besoin) si tu veux connaitre sa traduction\`\`\`` });
+            }else{
+              await Thread.send({ content: `\`\`\`${err}\`\`\`` });
+            }
+            return;
           });
       }
     };
-    
+
     if (!Channels.includes(message.channel.id)) {
       Channels.push(message.channel.id);
       if (!Started) {
         bot.on("messageCreate", listener);
-        Started=true;
+        Started = true;
       }
       return message.reply({ content: "Auto trad activée sur ce canal" });
     } else {
